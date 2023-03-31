9fc11fed3561c6021f9e2f711bbe7317c9fbac8e
Darya <d.a_alpatskaya@mail.ru>
2023-03-31 17:51:09 +0300

    Добавили решение 23  задачи

--- Задача 23/Program.cs
+++ Задача 23/Program.cs
@@ -8,16 +8,10 @@
 
 Console.Clear();
 Console.Write("Введите число N: ");
-int N = new int[N];
-// n=5
-// 0 1 2 3 4 
-// 0 0 0 0 0
-//1 2 3 4 5
-//1 4 9 16 25
+int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
  for (int i = 1; i <= N; i++)
-    result[i - 1] = i * i;
+    result[i - 1] = i * i * i;
    
-    for (int i = 0; i < N; i++)
-Console.Write($"{result[i]} ");
     for (int i = 0; i < N; i++)
 Console.Write($"{result[i]} ");
