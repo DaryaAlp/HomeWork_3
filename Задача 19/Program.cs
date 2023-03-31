e8da642980c34c249c2f9aa845a7761af9e9d465
Darya <d.a_alpatskaya@mail.ru>
2023-03-31 17:53:56 +0300

    Откорректировали 19 задачу

--- Задача 19/Program.cs
+++ Задача 19/Program.cs
@@ -9,7 +9,7 @@
 Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine());
-if ((number/10000 == number %10) || ((number /1000) % 10 == (number%100)/10)){
+if ((number /1000) % 10 == (number%100)/10){
     Console.WriteLine ("Число является палиндромом");}
 else {
     Console.WriteLine ("Это число не палиндром");  
